810b625154d4b73df3504be25266f62b7f5c0a68
jamiebdavis <jamied1985@gmail.com>
2020-03-15 10:12:33 +0000

    finished

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,13 +1,35 @@
 import React from "react";
 
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
+    <div style={styles.recipe}>
       <h1>{title}</h1>
+      {ingredients.map(ingredient => (
+        <li>{ingredient.text}</li>
+      ))}
       <p>Calories: {calories.toFixed(0)}</p>
-      <img src={image} alt="" />
+      <img style={styles.image} src={image} alt="" />
     </div>
   );
 };
 
 export default Recipe;
+
+const styles = {
+  recipe: {
+    borderRadius: "10px",
+    boxShadow: "0px 5px 20px grey",
+    margin: "20px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    background: "white",
+    alignItems: "center",
+    minWidth: "40px"
+  },
+  image: {
+    borderRadius: "50%",
+    width: "100px",
+    height: "100px"
+  }
+};
